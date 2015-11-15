f670b493c84610beeb2b6dffe238162d9a89cf13
Jacob Whitehill <jacob_whitehill@harvard.edu>
2015-11-15 13:57:39 -0500

    Renamed field name.

--- visualize_coefs.r
+++ visualize_coefs.r
@@ -9,7 +9,7 @@ for (j in 1:length(disciplines)) {
 	discipline <- disciplines[j]
 
 	models <- vector(mode="list", length=3)
-	comparisons <- c("0_v_MoreThan0", "1_v_MoreThan1", "1OrMore_v_4OrMore")
+	comparisons <- c("0_v_MoreThan0", "1_v_MoreThan1", "AtMost4_v_4OrMore")
 	names(models) <- comparisons
 
 	d$take0OrMore <- d[paste("numCourses", discipline, sep="")] > 0
