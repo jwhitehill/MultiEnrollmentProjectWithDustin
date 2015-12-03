6d749431cea760caf1080c73dee77d8408f29ee1
Jacob Whitehill <jacob_whitehill@harvard.edu>
2015-12-03 14:19:44 -0500

    Re-worked create_dataset.py so that it just removes columns and anonymizes the person/course dataset.

--- create_dataset.py
+++ create_dataset.py
@@ -34,20 +34,27 @@ def getEnrollmentsAndDisciplines ():
 	allDisciplines = np.unique(courseIdMap.values())
 	return d, disciplines, allDisciplines
 
-def splitAndWrite (e, allDisciplines, trainFrac, testFrac, trainFilename, testFilename, holdoutFilename, columns):
-	# Divide into train, test, and holdout
-	idxs = np.random.permutation(len(e))
-	numTrainIdxs = int(trainFrac * len(idxs))
-	numTestIdxs = int(testFrac * len(idxs))
-	trainIdxs = idxs[0:numTrainIdxs]
-	testIdxs = idxs[numTrainIdxs:numTrainIdxs+numTestIdxs]
-	holdoutIdxs = idxs[numTrainIdxs+numTestIdxs:]
+def splitAndWrite (d, trainFrac, testFrac, trainFilename, testFilename, holdoutFilename, columns):
+	# Divide user_id's into train, test, and holdout sets.
+	userIds = np.unique(d.user_id)
+	numTrainIdxs = int(trainFrac * len(userIds))
+	numTestIdxs = int(testFrac * len(userIds))
+	def getSet (i):
+		if i < numTrainIdxs:
+			return 0
+		elif i < numTrainIdxs+numTestIdxs:
+			return 1
+		else:
+			return 2
+	idxs = np.random.permutation(len(userIds))
+	userIdMap = { userIds[i]:getSet(idxs[i]) for i in range(len(userIds)) }  # Use "idxs" to randomize
+	setAssignments = d.user_id.map(userIdMap)
 
 	# Write each subset to disk
-	f = pandas.DataFrame(e, columns=columns)
-	f.iloc[trainIdxs].to_csv(trainFilename, index=False)
-	f.iloc[testIdxs].to_csv(testFilename, index=False)
-	f.iloc[holdoutIdxs].to_csv(holdoutFilename, index=False)
+	e = pandas.DataFrame(d, columns=columns)
+	e.iloc[np.nonzero(setAssignments == 0)[0]].to_csv(trainFilename, index=False)
+	e.iloc[np.nonzero(setAssignments == 1)[0]].to_csv(testFilename, index=False)
+	e.iloc[np.nonzero(setAssignments == 2)[0]].to_csv(holdoutFilename, index=False)
 
 def convertStartTimes (d):
 	goodIdxs = []
@@ -69,50 +76,20 @@ def convertStartTimes (d):
 def createIndividualEnrollmentDataset ():
 	d, disciplines, allDisciplines = getEnrollmentsAndDisciplines()
 	d = convertStartTimes(d)
-	fields = [ 'course_id', 'certified', 'start_time', 'user_id' ] + common.DEMOGRAPHIC_FIELDS
+	fields = [ 'course_id', 'explored', 'start_time', 'user_id', 'ndays_act'  ] + common.DEMOGRAPHIC_FIELDS
 	d = d[fields]
 
 	# Remove all rows containing any NaNs
 	idxs = np.nonzero(np.sum(pandas.isnull(d), axis=1) == 0)[0]
 	d = d.iloc[idxs]
 
-	# Construct matrix of who started each course and when, along with whether he/she certified in each course
-	uniqueCourseIds = np.unique(d.course_id)
-	courseIdsIdxsMap = { uniqueCourseIds[i]:i for i in range(len(uniqueCourseIds)) }
+	# Remove identifying information by mapping from username to an integer
+	userIds = np.unique(d.user_id)
+	userIds = userIds[np.random.permutation(len(userIds))]
+	userIdsMap = { userIds[i]:i for i in range(len(userIds)) }
+	d.user_id = d.user_id.map(userIdsMap)
 
-	# Create maps from userId to demographics, and from userId to their indices in a table
-	userIdsDemographics = d.drop_duplicates(subset="user_id")
-	allUserIds = list(userIdsDemographics.user_id)
-	userIdsDemographics = userIdsDemographics[common.DEMOGRAPHIC_FIELDS]
-	userIdsDemographics = userIdsDemographics.as_matrix()
-	userIdsIdxsMap = { allUserIds[i]:i for i in range(len(allUserIds)) }
-	
-	# Now create a table of which users started/certified in which courses before/after time T
-	e = np.zeros((len(userIdsIdxsMap.keys()), len(uniqueCourseIds) * 3), dtype=np.int32)
-	T = np.datetime64('2015-06-30')  # Arbitrary cutoff time
-	for i in range(d.shape[0]):
-		if i % 1000 == 0:
-			print i
-		userId = d.iloc[i].user_id
-		courseId = d.iloc[i].course_id
-		certified = d.iloc[i].certified
-		startTime = d.iloc[i].start_time
-		courseIdx = courseIdsIdxsMap[courseId]
-		e[userIdsIdxsMap[userId]][3*courseIdx+0] = startTime < T
-		e[userIdsIdxsMap[userId]][3*courseIdx+1] = startTime >= T
-		e[userIdsIdxsMap[userId]][3*courseIdx+2] = certified
-
-	# Now concatenate the two tables
-	f = np.concatenate((userIdsDemographics, e), axis=1)
-
-	# Split and write to disk
-	columns = list(common.DEMOGRAPHIC_FIELDS)
-	for i in range(len(uniqueCourseIds)):
-		courseName = uniqueCourseIds[i].replace(" ", "").replace("/", "_")
-		columns.append("start_" + courseName + "_beforeT")
-		columns.append("start_" + courseName + "_afterT")
-		columns.append("certified" + courseName)
-	splitAndWrite(f, allDisciplines, 0.7, 0.2, "train_individual_courses.csv", "test_individual_courses.csv", "holdout_individual_courses.csv", columns)
+	splitAndWrite(d, 0.7, 0.2, "train_individual.csv", "test_individual.csv", "holdout_individual.csv", d.columns)
 
 def createAggregateEnrollmentDataset ():
 	d, disciplines, allDisciplines = getEnrollmentsAndDisciplines()
@@ -144,7 +121,7 @@ def createAggregateEnrollmentDataset ():
 			print count
 
 	columns = fields + [ ('numCourses'+allDisciplines[i].replace(' ', '')) for i in range(len(allDisciplines)) ] + [ 'numCoursesAll' ]
-	splitAndWrite(e, allDisciplines, 0.7, 0.2, "train.csv", "test.csv", "holdout.csv", columns)
+	splitAndWrite(e, 0.7, 0.2, "train.csv", "test.csv", "holdout.csv", columns)
 
 if __name__ == "__main__":
 	#createAggregateEnrollmentDataset()
