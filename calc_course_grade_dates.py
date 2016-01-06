c6f9077d89baeaa2431ed126d962d6338eb61371
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-01-06 14:03:32 -0500

    Added script to calculate by when a student could have earned a certificate in each course.

---
+++ calc_course_grade_dates.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pandas
+
+DIR = "/nfs/home/J/jwhitehill/shared_space/ci3_jwaldo/dseaton/For_Jake"
+FRAC = 0.5
+
+def computeDate (f, frac):
+	d = pandas.read_csv(f)
+	if d.shape[0] == 0:
+		return None
+	threshold = d.threshold
+	idxs = np.nonzero(d['Csum Pts'] > threshold * frac)
+	date = np.min(d.start.iloc[idxs])
+	return date
+
+if __name__ == "__main__":
+	files = os.listdir(DIR)
+	courseDateMap = {}
+	for f in files:
+		if ".csv" in f:
+			idx = f.index("_ideal_grade.csv")
+			course = f[0:idx].replace("__", "/")
+			filename = DIR + "/" + f
+			courseDateMap[course] = computeDate(filename, FRAC)
