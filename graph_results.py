a22bdd0d0fcf1d6c7721c4522de4c8648eaf59f5
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-02-04 13:35:34 -0500

    Wrote code to plot results.

---
+++ graph_results.py
@@ -0,0 +1,40 @@
+import cPickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+resultsCertRepeatedCourse = cPickle.load(open("results_prong1.pkl", "rb"))
+(resultsNextWeek, resultsCert) = cPickle.load(open("results_prong2.pkl", "rb"))
+
+for courseId in resultsCertRepeatedCourse.keys():
+	plt.clf()
+
+	someResultsCertRepeatedCourse = np.array(resultsCertRepeatedCourse[courseId])
+	someResultsNextWeek = np.array(resultsNextWeek[courseId])
+	someResultsCert = np.array(resultsCert[courseId])
+
+	# Results from prong #1 are from weeks 1 and later; hence, "+ 1"
+	# Results from prong #2 are from weeks 3 and later; hence, "+ 3"
+	names = []
+	handles = []
+	if len(someResultsCertRepeatedCourse) > 0:
+		handle, = plt.plot(np.arange(len(someResultsCertRepeatedCourse)) + 1, someResultsCertRepeatedCourse*100., 'c-.')
+		handles.append(handle)
+		plt.plot(np.arange(len(someResultsCertRepeatedCourse)) + 1, someResultsCertRepeatedCourse*100., 'co')
+		names.append("Certify (trained from prev. course)")
+	if len(someResultsCertRepeatedCourse) > 0:
+		handle, = plt.plot(np.arange(len(someResultsNextWeek)) + 3, someResultsNextWeek*100., 'k-')
+		handles.append(handle)
+		plt.plot(np.arange(len(someResultsNextWeek)) + 3, someResultsNextWeek*100., 'ko')
+		names.append("Persist to next week")
+	if len(someResultsCert) > 0:
+		handle, = plt.plot(np.arange(len(someResultsCert)) + 3, someResultsCert*100., 'm-')
+		handles.append(handle)
+		plt.plot(np.arange(len(someResultsCert)) + 3, someResultsCert*100., 'mo')
+		names.append("Certify")
+	plt.legend(handles, names, loc="lower right")
+	plt.title(courseId)
+	plt.xlabel("Week #")
+	plt.ylabel("Accuracy (%)")
+	plt.xlim((0., 7.))
+	plt.ylim((50., 100.))
+	plt.savefig("{}_graph.png".format(courseId.replace("/", "-")))
